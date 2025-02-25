aee90d30061060424e0b2cddfb4445896f78d011
omgdory <dorianakhavan@icloud.com>
2025-02-25 13:24:41 -0800

    Update printing

--- x86toCPP/AST/GlobalDeclaratorNode.cs
+++ x86toCPP/AST/GlobalDeclaratorNode.cs
@@ -12,7 +12,7 @@ public class GlobalDeclaratorNode : ASTNode {
 
   // print to screen
   public override void Print(int indent = 0) {
-    Console.WriteLine($"{new string(' ', indent)}Global:{GlobalToken.Value} , {TargetToken.Value}");
+    Console.WriteLine($"{new string(' ', indent)}Global: {GlobalToken.Value} , {TargetToken.Value}");
     PrintChildren(indent);
   }
 }

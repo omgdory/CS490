04b3380eb101db215531ac0c9efbbae3818ae6c5
omgdory <dorianakhavan@icloud.com>
2025-03-10 22:20:27 -0700

    NameChecker framework

---
+++ x86toCPP/Visitor/NameChecker.cs
@@ -0,0 +1,44 @@
+namespace x86toCPP;
+
+public class NameChecker : Visitor {
+  public void visitRoot(RootNode node) {
+
+  }
+
+  public void visitDefault(DefaultNode node) {
+
+  }
+
+  public void visitDataDirective(DataDirectiveNode node) {
+
+  }
+
+  public void visitGlobalDeclarator(GlobalDeclaratorNode node) {
+
+  }
+
+  public void visitLabel(LabelNode node) {
+
+  }
+
+  public void visitMacro(MacroNode node) {
+
+  }
+
+  public void visitMemoryAccess(MemoryAccessNode node) {
+
+  }
+
+  public void visitMnemonic(MnemonicNode node) {
+
+  }
+
+  public void visitOperand(OperandNode node) {
+
+  }
+
+  public void visitSegment(SegmentNode node) {
+
+  }
+
+}
